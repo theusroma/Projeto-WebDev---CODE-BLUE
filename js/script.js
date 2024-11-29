3a6a335a6475fcf42f4ee341f62b8cebe4eebf1c
j0a0cesar <jcpaduatimoteo09@gmail.com>
2024-11-29 16:49:05 -0300

    primeiro versionamento do code blue

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
    /* BOTOES */
-    let env = document.getElementById("botaoEnviar");
-    let doe = document.getElementById("botaoDoar");
+   let env = document.getElementById("botaoEnviar");
+   let doe = document.getElementById("botaoDoar");
 
-    doe.addEventListener("click", doar);
+   doe.addEventListener("click", doar);
 
-    function doar(){
-        let chavePix = prompt("Por favor, insira sua chave Pix: ");
-        if(chavePix){
-            const quantidade = prompt("Por favor, insira a quantidade que deseja doar:");
-            if (quantidade) {
-                alert("Doação enviada com sucesso! Muito obrigado.");
-            } else {
-                alert("Doação cancelada.");
-            }
-        } else {
-            alert("Doação cancelada.");
-        }
-    }
+   function doar(){
+       let chavePix = prompt("Por favor, insira sua chave Pix: ");
+       if(chavePix){
+           const quantidade = prompt("Por favor, insira a quantidade que deseja doar:");
+           if (quantidade) {
+               alert("Doação enviada com sucesso! Muito obrigado.");
+           } else {
+               alert("Doação cancelada.");
+           }
+       } else {
+           alert("Doação cancelada.");
+       }
+   }
